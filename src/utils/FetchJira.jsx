c27f09348954019a7b6d474bf4c091695a52e205
Jonathan <u605592@teco.com.ar>
2022-08-25 17:42:27 -0300

    fix bug CORS

--- src/utils/FetchJira.jsx
+++ src/utils/FetchJira.jsx
@@ -1,68 +1,22 @@
+import axios from "axios";
 const api = "https://gestioncio.telecom.com.ar/rest/api/2/";
 const token = "ODU2OTMyNjk1MDEwOpyggcSWAysOsED4PTS5vYc/pbMd";
 const getIssue = "issue/";
 
 
 
-
+let config = {
+  headers: {
+    'Authentication': 'OAuth 2.0',
+    'Authorization': 'Bearer ODU2OTMyNjk1MDEwOpyggcSWAysOsED4PTS5vYc/pbMd',
+    'Content-Type': 'application/json'
+  }
+}
 
 export const FetchIssue = async () => {
-  let reponse = await fetch('https://gestioncio.telecom.com.ar/rest/api/2/issue/ODT-11', {
-    headers: {
-      'Authentication': 'OAuth 2.0',
-      'Authorization': 'Bearer ODU2OTMyNjk1MDEwOpyggcSWAysOsED4PTS5vYc/pbMd',
-      'Content-Type': 'application/json',
-
-      
-    },
-    method: 'GET',
-    })
-  let JSONreponse = await reponse.json();
- 
-  if (reponse.ok) {
-    return JSONreponse;
-  }else {
-      alert("error al obtener el catalogo");
-      return false;    
-  }
+  let reponse = await axios.get('rest/api/2/issue/ODT-11', config)
+    return reponse.data;
   
-
-}
-
-export const FetchCourseList = async () => {
-  let reponse = await fetch('https://hackathon-telecom.herokuapp.com/api/cursos', {
-                          headers: {
-                              'Content-Type': 'application/json'
-                          },
-                          method: 'GET',
-                          })
-  let JSONreponse = await reponse.json();
-   
-  if (reponse.ok) {
-           console.log(JSONreponse);
-           return JSONreponse;
-   }else {
-          alert("error al obtener el catalogo");
-          return false;
-          
-   }
 }
 
 
-export const FetchCourse = async (idCourse) => {
-  let reponse = await fetch('https://hackathon-telecom.herokuapp.com/api/cursos/'+idCourse, {
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      method: 'GET',
-      })
-  let JSONreponse = await reponse.json();
-
-  if (reponse.ok) {
-      return JSONreponse;
-  }else {
-      alert("error al obtener el catalogo");
-  return false;
-
-}
-}
